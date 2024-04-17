09b52eb8d2297263d598c1652165263ded3b38dd
joosthb <joost@hendricksen.net>
2024-04-17 16:20:36 +0200

    cleaned up

--- build.sh
+++ build.sh
@@ -8,16 +8,35 @@ echo -n "[empty]" > data/intermediate_ca/index
 echo -n "00" > data/root_ca/serial
 echo -n "00" > data/intermediate_ca/serial
 
-openssl genrsa -out data/root_ca/root.key 2048
-openssl req -new -key data/root_ca/root.key -out data/root_ca/root.csr -config root_req.config
-openssl ca -in data/root_ca/root.csr -out data/root_ca/root.pem -config root.config -selfsign -extfile ca.ext -days 1095
+# Root CA
+openssl req -newkey rsa:2048 -noenc \
+    -keyout data/root_ca/root.key \
+    -out data/root_ca/root.csr \
+    -subj "/C=NL/CN=Test Root CA"
+openssl ca -batch -in data/root_ca/root.csr \
+    -out data/root_ca/root.pem \
+    -config root.config \
+    -selfsign -extfile ca.ext -days 1095
 
-openssl genrsa -out data/intermediate_ca/intermediate.key 2048
-openssl req -new -key data/intermediate_ca/intermediate.key -out data/intermediate_ca/intermediate.csr -config intermediate_req.config
-openssl ca -in data/intermediate_ca/intermediate.csr -out data/intermediate_ca/intermediate.pem -config root.config -extfile ca.ext -days 730
+# Intermediate CA
+openssl req -newkey rsa:2048 -noenc \
+    -keyout data/intermediate_ca/intermediate.key \
+    -out data/intermediate_ca/intermediate.csr \
+    -subj "/C=NL/CN=Test Intermediate CA"
+openssl ca -batch -in data/intermediate_ca/intermediate.csr \
+    -out data/intermediate_ca/intermediate.pem \
+    -config root.config -extfile ca.ext -days 730
 
-openssl genrsa -out data/leaf.key 2048
-openssl req -new -key data/leaf.key -out data/leaf.csr -config leaf_req.config
-openssl ca -in data/leaf.csr -out data/leaf.pem -config intermediate.config -days 365
+# Leaf certificate
+openssl req -newkey rsa:2048 -noenc \
+    -keyout data/leaf.key \
+    -out data/leaf.csr \
+    -subj "/C=NL/CN=Test Leaf"
+openssl ca -batch -in data/leaf.csr \
+    -out data/leaf.pem \
+    -config intermediate.config -days 365
 
+# TODO expand fields later: "/C=NL/ST=GE/L=Nijmegen/O=server/CN=server.localhost"
+
+# Verify certs
 openssl verify -x509_strict -CAfile data/root_ca/root.pem -untrusted data/intermediate_ca/intermediate.pem data/leaf.pem
